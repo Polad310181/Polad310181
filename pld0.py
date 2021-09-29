a9ff8c34c8face8478d40fc3e2a4b1407fc692b7
Polad310181 <91432702+Polad310181@users.noreply.github.com>
2021-09-29 15:55:49 +0300

    Add files via upload

---
+++ pld0.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 17 13:35:22 2021
+
+@author: Casper
+"""
+
+veriseti = pd.read_csv (r'C:\Users\Casper\Desktop\Ann.csv')
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import stats
+from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
